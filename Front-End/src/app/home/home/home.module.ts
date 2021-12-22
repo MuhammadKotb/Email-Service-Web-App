430721ac2a882d891c51109c377362cec98160da
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2021-12-22 15:13:59 +0200

    home page routing & adding additional outlet & add components

--- Front-End/src/app/home/home/home.module.ts
+++ Front-End/src/app/home/home/home.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SentComponent } from './sent/sent/sent.component';
+import { TrashComponent } from './trash/trash/trash.component';
+import { ContactsComponent } from './contacts/contacts/contacts.component';
+import { SendEmailComponent } from './sendEmail/send-email/send-email.component';
+import { InboxComponent } from './inbox/inbox/inbox.component';
+import { DraftComponent } from './draft/draft/draft.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SentComponent,
+    TrashComponent,
+    ContactsComponent,
+    SendEmailComponent,
+    InboxComponent,
+    DraftComponent
+  ],
   imports: [
     CommonModule
+
   ]
 })
 export class HomeModule { }
