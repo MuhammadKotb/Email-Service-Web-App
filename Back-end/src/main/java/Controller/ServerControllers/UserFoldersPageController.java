a9ebc56a340fd442ac53a18c34aaabedc861dcf8
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2021-12-28 17:41:22 +0200

    refactoring

--- Back-end/src/main/java/Controller/ServerControllers/UserFoldersPageController.java
+++ Back-end/src/main/java/Controller/ServerControllers/UserFoldersPageController.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-public class profileFolderController {
+public class UserFoldersPageController {
 
     @PostMapping("/addUserFolder")
     String addUserFolder(@RequestParam(value="foldername") String folderName, @RequestParam("username") String username){
